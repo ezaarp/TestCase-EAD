cf55d2e542d690d011d3a7034fbf0140cbec1af5
ezaarp <andrariezarizqip@gmail.com>
2025-08-10 21:04:43 +0700

    Add required to fill create form

--- form_create_barang.php
+++ form_create_barang.php
@@ -16,19 +16,19 @@
                         <form action="create.php" method="POST" enctype="multipart/form-data" name="form_create">
                             <!-- Lengkapi form dengan inputan yang dibutuhkan -->
                             <div class="form-floating mb-3">
-                                <input type="string" class="form-control" id="nama_barang" name="nama_barang">
+                                <input type="string" class="form-control" id="nama_barang" name="nama_barang" required>
                                 <label for="nama_barang">Nama Barang</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input type="string" class="form-control" id="jenis_barang" name="jenis_barang">
+                                <input type="string" class="form-control" id="jenis_barang" name="jenis_barang" required>
                                 <label for="jenis_barang">Jenis Barang</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input type="string" class="form-control" id="lokasi_barang" name="lokasi_barang">
+                                <input type="string" class="form-control" id="lokasi_barang" name="lokasi_barang" required>
                                 <label for="lokasi_barang">Lokasi Terakhir/TKP</label>
                             </div>
                             <div class="form-floating mb-3">
-                                <input type="string" class="form-control" id="kontak" name="kontak">
+                                <input type="string" class="form-control" id="kontak" name="kontak" required>
                                 <label for="kontak">Kontak Pemilik/Penemuan</label>
                             </div>
                             <button type="submit" class="btn btn-primary mb-3 mt-3 w-100">Tambah</button>
